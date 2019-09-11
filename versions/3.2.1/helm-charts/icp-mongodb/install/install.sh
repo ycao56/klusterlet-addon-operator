88b7d3b5e3979da550cd68c914b1995d186272c3
Tony Tran <TonyTran@ibm.com>
2019-09-10 21:48:35 -0400

    update copyright (#74)
    * topology component
    
    * WEIP
    
    * fix issue with deploying certmgr
    
    * Update test.sh
    
    Standardizing newline convention for shebang
    
    * Update build-helm-operator-image.sh
    
    Standardizing newline convention for shebang
    
    * Update copyright-check.sh
    
    Standardizing newline convention for shebang
    
    * Update install-operator-sdk.sh
    
    Standardizing newline convention for shebang
    
    * Update slack-notification.sh
    
    Standardizing newline convention for shebang
    
    * Update update-copyright.sh
    
    Standardizing newline convention for shebang
    
    * Update travis-env.sh
    
    Standardizing newline convention for shebang
    
    * Update mongodb-up-test.sh
    
    Standardizing newline convention for shebang
    
    * Should not be committed
    
    * Should not be committed to repo
    
    * Fixes to duplicate licenses

--- versions/3.2.1/helm-charts/icp-mongodb/install/install.sh
+++ versions/3.2.1/helm-charts/icp-mongodb/install/install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Licensed Materials - Property of IBM
+# 5737-E67
+# (C) Copyright IBM Corporation 2016, 2019 All Rights Reserved
+# US Government Users Restricted Rights - Use, duplication or disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
 
 # Copyright 2016 The Kubernetes Authors. All rights reserved.
 #
--- versions/3.2.1/helm-charts/icp-mongodb/mongodb-up-test.sh
+++ versions/3.2.1/helm-charts/icp-mongodb/mongodb-up-test.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# Licensed Materials - Property of IBM
+# 5737-E67
+# (C) Copyright IBM Corporation 2016, 2019 All Rights Reserved
+# US Government Users Restricted Rights - Use, duplication or disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+
 
     MONGOCACRT=/ca/tls.crt
     MONGOPEM=/work-dir/mongo.pem
