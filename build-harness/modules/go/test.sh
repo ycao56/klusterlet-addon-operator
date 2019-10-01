7f83575f1e073d6d2baca999e207bc87a8ade5dd
Christopher D Compton <compton6@us.ibm.com>
2019-10-01 09:40:14 -0400

    Remove build harness as project is building again with correct credentials (#86)

--- build-harness/modules/go/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-_script_dir=$(dirname "$0")
-mkdir -p test/coverage
-echo 'mode: atomic' > test/coverage/cover.out
-echo '' > test/coverage/cover.tmp
-echo -e "${GOPACKAGES// /\\n}" | xargs -n1 -I{} $_script_dir/test-package.sh {} ${GOPACKAGES// /,}
